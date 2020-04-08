b167462a3ff7fcfd8bdccb4e5de62ef122776a00
VHubert <valentin.huberts@laposte.net>
2020-04-08 10:10:04 +0200

    Unit tests for empty args and help command

--- src/parse.rs
+++ src/parse.rs
@@ -5,15 +5,35 @@ pub struct Config {
     cert_file: String
 }
 
+#[derive(PartialEq, Debug)]
 pub enum Command {
     Login { email: String },
     Deploy { group: String, artifact: String, name: String},
     GetId { serial_number: String },
-    GetInfo { id: String }
+    GetInfo { id: String },
+    Help
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
         Err("oops")
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn no_args() {
+        let args = vec![String::from("name")];
+        assert!(Config::new(&args).is_err());
+    }
+
+    #[test]
+    fn help_cmd() {
+        let args = vec![String::from("name"), String::from("help")];
+        let config = Config::new(&args).unwrap();
+        assert_eq!(config.command, Command::Help);
     }
 }
