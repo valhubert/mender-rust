165d370ab2ef420406a228a484ae019e1cc0f367
VHubert <valentin.huberts@laposte.net>
2020-04-08 09:51:55 +0200

    Fill Config struct and create Command enum

--- src/parse.rs
+++ src/parse.rs
@@ -1,9 +1,19 @@
 pub struct Config {
+    command: Command,
+    token: String,
+    server_url: String,
+    cert_file: String
+}
 
+pub enum Command {
+    Login { email: String },
+    Deploy { group: String, artifact: String, name: String},
+    GetId { serial_number: String },
+    GetInfo { id: String }
 }
 
 impl Config {
     pub fn new(args: &[String]) -> Result<Config, &'static str> {
-        Ok(Config {})
+        Err("oops")
     }
 }
