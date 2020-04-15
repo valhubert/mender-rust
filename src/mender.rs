2043f6032814fbbcb823155b4c8d3fa00f90bbe9
VHubert <valentin.huberts@laposte.net>
2020-04-15 10:43:40 +0200

    Properly handle errors and success in get_token

--- src/mender.rs
+++ src/mender.rs
@@ -1,27 +1,60 @@
 use super::parse::{Command, Config};
 use std::error::Error;
+use std::fmt::Display;
 
 pub const LOGIN_API: &str = "/api/management/v1/useradm/auth/login";
 
-/// Request an auth token from mender server
+#[derive(Debug)]
+pub struct MenderError {
+    err: String,
+}
+
+impl Display for MenderError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::result::Result<(), std::fmt::Error> {
+        write!(f, "{}", self.err)
+    }
+}
+
+impl Error for MenderError {}
+
+impl MenderError {
+    fn new(err: String) -> MenderError {
+        MenderError { err }
+    }
+}
+
+fn blocking_client(
+    cert: Option<reqwest::Certificate>,
+) -> reqwest::Result<reqwest::blocking::Client> {
+    reqwest::blocking::Client::builder()
+        .danger_accept_invalid_certs(true)
+        .build()
+}
+
+/// Request an auth token from mender server, it should be called
+/// with a Login command otherwise an error is returned.
 pub fn get_token(conf: &Config, pass: &str) -> Result<String, Box<dyn Error>> {
     // WARNING: should add cert from Config instead of ignoring
     // all errors !!
     if let Command::Login { email } = &conf.command {
-        let client = reqwest::blocking::Client::builder()
-            .danger_accept_invalid_certs(true)
-            .build()?;
+        let client = blocking_client(None)?;
         let url_login = conf.server_url.clone() + LOGIN_API;
         let get_token = client
             .post(&url_login)
             .basic_auth(&email, Some(pass))
             .send()?;
-        // TODO: check status code, if 200 return OK with token
-        // if not parse JSON to get the error (ex: unauthorized)
-        println!("{}", get_token.text().unwrap());
+        if get_token.status().is_success() {
+            Ok(get_token.text().unwrap())
+        } else {
+            Err(Box::new(MenderError::new(format!(
+                "login error. Status code '{}' response '{}'",
+                get_token.status(),
+                get_token.text().unwrap()
+            ))))
+        }
     } else {
-        // Error
+        Err(Box::new(MenderError::new(String::from(
+            "Command must be Login for get_token",
+        ))))
     }
-
-    Ok(String::from("totoken"))
 }
